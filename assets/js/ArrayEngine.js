f1807019ec0d65be215b64fbb609affa68089475
Jonathan Perry <sequencemedia@sequencemedia.net>
2012-12-16 04:41:19 -0800

    Added "bite".

--- assets/js/ArrayEngine.js
+++ assets/js/ArrayEngine.js
@@ -50,7 +50,7 @@ var ArrayEngine	= (function () {
 		 *	Performs moderately slower than "DifferenceEngine.indexFor()"
 		 */
 
-		return function indexOf(key) {
+		return function (key) {
 
 			if (KEY === key) {
 
@@ -95,7 +95,7 @@ var ArrayEngine	= (function () {
 		if ((array || false).constructor === Array) {
 
 			i	= 0;
-			j	= (ARRAY = array).length;
+			j	= (ARRAY = array.slice()).length;
 			n	= -1;
 			m	= -1;
 
@@ -120,11 +120,116 @@ var ArrayEngine	= (function () {
 
 	}
 
+	function bite(x, y) {
+
+		var a = ARRAY, max = Math.max, min = Math.min, pow = Math.pow, N = Number.NEGATIVE_INFINITY, l = 0, u = (j - 1);
+
+		if (typeof x === "number") {
+
+			if (typeof y === "number") {
+
+				/*
+				 *	Negative zero slices lower bound 0
+				 *
+				 *	Massage x and y to acceptable values
+				 *	l is lower bound 0
+				 *	u is upper bound (j - 1)
+				 *
+				 *	Massage the second argument to slice
+				 */
+				return pow(x, m) === N && pow(y, m) === N ? a.slice(0).reverse() : (x = max(l, min(u, x > m ? x : x + u))) > (y = min(u, max(l, y > m ? y : y + u))) ? a.slice(y, x + 1).reverse() : a.slice(x, y + 1);
+
+			}
+
+			/*
+			 *	Negative zero slices upper bound (j - 1)
+			 *	Positive zero slices lower bound 0
+			 *
+			 *	Massage x to an acceptable value
+			 *	l is lower bound 0
+			 *	u is upper bound (j - 1)
+			 */
+			return pow(x, m) === N ? a.slice(u) : a.slice(max(l, min(u, x > m ? x : x + u)));
+
+		}
+
+		return [];
+
+	}
+
+	/*
+	function bite(x, y) {
+
+		var a = ARRAY, max = Math.max, min = Math.min, pow = Math.pow, N = Number.NEGATIVE_INFINITY, l = 0, u = (j - 1);
+
+		if (typeof x === "number") {
+
+			if (typeof y === "number") {
+
+				if ((pow(x, m) === N) && (pow(y, m) === N)) {
+					*/
+					/*
+					 *	Negative zero slices lower bound 0
+					 *//*
+					return a.slice(0).reverse();
+
+				} else {
+					*/
+					/*
+					 *	Massage x and y to acceptable values
+					 *	l is lower bound 0
+					 *	u is upper bound (j - 1)
+					 *//*
+					x = max(l, min(u, x > m ? x : x + u));
+					y = min(u, max(l, y > m ? y : y + u));
+					*/
+					/*
+					 *	Massage the second argument to slice
+					 *//*
+					return x > y ? a.slice(y, x + 1).reverse() : a.slice(x, y + 1);
+
+				}
+
+			}
+
+			if (pow(x, m) === N) {
+				*/
+				/*
+				 *	Negative zero slices upper bound (j - 1)
+				 *	Positive zero slices lower bound 0
+				 *//*
+				return a.slice(u);
+
+			} else {
+				*/
+				/*
+				 *	Massage x to an acceptable value
+				 *	l is lower bound 0
+				 *	u is upper bound (j - 1)
+				 *//*
+				return a.slice(max(l, min(u, x > m ? x : x + u)));
+
+			}
+
+		}
+
+		return [];
+
+	}
+	*/
+
+	/*
+	 * Safari performs significantly faster with anonymous functions returned from
+	 * the self-executing function according to jsperf.com, December 2012. FF and
+	 * Chrome moderately favour named functions (but the difference to Safari is an
+	 * order of magnitude)
+	 */
+
 	map = (function () {
 
 		var z, array;
 
-		return function map(method) {
+		return function (method) {
 
 			if ((method || false).constructor === Function) {
 
@@ -148,6 +253,12 @@ var ArrayEngine	= (function () {
 
 	}());
 
+	/*
+	 * While array.sort().pop() performs significantly faster at identifying the "largest"
+	 * string value than an iterative comparison, using array.slice() to duplicate the array
+	 * negates that benefit
+	 */
+
 	max	= (function () {
 
 		var x, y, z;
@@ -156,7 +267,7 @@ var ArrayEngine	= (function () {
 
 			x = ARRAY[0];
 			z = 1;
-			do { //console.log("MAX", z, ARRAY[z], x, ARRAY[z] > x);
+			do {
 
 				if ((y = ARRAY[z]) > x) x = y;
 
@@ -165,13 +276,19 @@ var ArrayEngine	= (function () {
 
 		}
 
-		return function max() {
+		return function () {
 
 			return isNaN(z = Math.max.apply(ARRAY, ARRAY)) ? MAX() : z ; //array.slice().sort().pop() : z ;
 
 		}
 
 	}());
+
+	/*
+	 * While array.sort().shift() performs significantly faster at identifying the "smallest"
+	 * string value than an iterative comparison, using array.slice() to duplicate the array
+	 * negates that benefit
+	 */
 
 	min	= (function () {
 
@@ -181,7 +298,7 @@ var ArrayEngine	= (function () {
 
 			x = ARRAY[0];
 			z = 1;
-			do { //console.log("MIN", z, ARRAY[z], x, ARRAY[z] < x);
+			do {
 
 				if ((y = ARRAY[z]) < x) x = y;
 
@@ -190,7 +307,7 @@ var ArrayEngine	= (function () {
 
 		}
 
-		return function min() {
+		return function () {
 
 			return isNaN(z = Math.min.apply(ARRAY, ARRAY)) ? MIN() : z ; //array.slice().sort().shift() : z ;
 
@@ -259,15 +376,15 @@ var ArrayEngine	= (function () {
 
 	function iterateBetween(x, y, method) {
 
-		var z;
+		var z, max = Math.max, min = Math.min;
 
 		if (typeof x === "number" && typeof y === "number" && (method || false).constructor === Function) {
 
 			if (x < y) {
 
-				x = Math.max(0, x);
-				y = Math.max(0, Math.min(j, y));
-				z = Math.min(j, (y + 1));
+				x = max(0, x);
+				y = max(0, min(j, y));
+				z = min(j, (y + 1));
 
 				do {
 
@@ -276,10 +393,6 @@ var ArrayEngine	= (function () {
 				} while (++x < z);
 
 				/*
-				x = Math.max(0, x);
-				y = Math.max(0, Math.min(j, y));
-				z = Math.min(j, (y + 1));
-
 				do {
 
 					if (method.call(ARRAY, x, ARRAY[x], y) === false)  {
@@ -293,9 +406,9 @@ var ArrayEngine	= (function () {
 
 			} else {
 
-				x = Math.max(m, Math.min(j, x));
-				y = Math.max(m, y);
-				z = Math.max(m, (y - 1));
+				x = max(m, min(j, x));
+				y = max(m, y);
+				z = max(m, (y - 1));
 
 				do {
 
@@ -304,10 +417,6 @@ var ArrayEngine	= (function () {
 				} while (z < --x);
 
 				/*
-				x = Math.max(m, Math.min(j, x));
-				y = Math.max(m, y);
-				z = Math.max(m, (y - 1));
-
 				do {
 
 					if (method.call(ARRAY, x, ARRAY[x], y) === false) {
@@ -341,6 +450,8 @@ var ArrayEngine	= (function () {
 
 	ArrayEngine.prototype.begin		= begin;
 	ArrayEngine.prototype.reset		= reset;
+
+	ArrayEngine.prototype.bite		= bite;
 
 	ArrayEngine.prototype.map		= map;
 	ArrayEngine.prototype.max		= max;
