21e1f143fe0a1ebce2dc9fd4f480cd94b44af48b
Jonathan Perry <sequencemedia@sequencemedia.net>
2014-06-14 16:45:14 +0100

    Whitespace. Added Temperature, Weight and Distance classes

--- assets/js/NumberEngine.js
+++ assets/js/NumberEngine.js
@@ -24,6 +24,9 @@ var NumberEngine	= (function () {
 	"use strict";
 
 	var numberEngine,
+		temperature,
+		weight,
+		distance,
 
 		OCT = 8,
 		DEC = 10,
@@ -44,26 +47,20 @@ var NumberEngine	= (function () {
 		 *	No implementation for negative fibonacci numbers (yet)
 		 */
 		function negative(n) {
-
 			throw "Not implemented";
 			return 0;
-
 		}
 
 		function positive(n) {
-
 			return (typeof (fibonacci = FIBONACCI[n]) === "number") ? fibonacci : (FIBONACCI[n] = (positive(n - 1) + positive(n - 2)));
-
 		}
 
 		return function (n) {
-
 			/*
 			 *	Conditions where truth is explicit generally perform faster than
 			 *	conditions where truthiness is implied; so (n > 0) is better than (n)
 			 */
 			return (typeof n === "number") ? (n > 0) ? positive(n) : 0 : NaN;
-
 		};
 
 	}());
@@ -74,17 +71,13 @@ var NumberEngine	= (function () {
 			cos 	= Math.cos,
 			atan2	= Math.atan2,
 			sqrt	= Math.sqrt,
-
 			PI 		= Math.PI;
 
 		function radians(n) {
-
 			return (n * PI) / 180;
-
 		}
 
 		function haversine(points, radius) {
-
 			var to,
 				from,
 				LAT,
@@ -92,64 +85,41 @@ var NumberEngine	= (function () {
 				lat,
 				lng,
 				a, c;
-
 			if ((points || false).constructor === Object) {
-
 				to 		= points.to;
 				from 	= points.from;
-
 				if ((to || false).constructor === Object && (from || false).constructor === Object) {
-
 					LAT 	= radians(to.lat - from.lat);
 					LNG 	= radians(to.lng - from.lng);
 					lat 	= LAT / 2;
 					lng 	= LNG / 2;
-
 					a = sin(lat) * sin(lat) + cos(radians(from.lat)) * cos(radians(to.lat)) * sin(lng) * sin(lng),
 					c = 2 * atan2(sqrt(a), sqrt(1 - a));
-
 					return (radius * c).toFixed(2);
-
 				}
-
 			}
-
 			return NaN;
-
 		}
 
 		return function (points) {
-
 			return {
-
 				km: function () {
-
 					return haversine(points, 6378);
-
 				},
-
 				mi: function () {
-
 					return haversine(points, 3963);
-
 				}
-
 			}
-
 		}
 
 	}());
 
 	function fromHexToDec(v) {
-
 		return (typeof v === "string") ? parseInt(v, HEX) : (typeof v === "number") ? v : NaN;
-
 	}
 
 	function fromOctToDec(v) {
-
 		return (typeof v === "string") ? parseInt(v, OCT) : (typeof v === "number") ? v : NaN;
-
 	}
 
 	/*
@@ -157,15 +127,29 @@ var NumberEngine	= (function () {
 	 */
 
 	function fromFToC(n) {
-
 		return (typeof n === "number") ? (n - 32) / 1.8 : NaN;
-
 	}
 
 	function fromCToF(n) {
-
 		return (typeof n === "number") ? (n * 1.8) + 32 : NaN;
+	}
 
+	function Temperature() {
+		return temperature || (this instanceof Temperature ? temperature = this : new Temperature());
+	}
+
+	Temperature.prototype.fromFToC = fromFToC;
+	Temperature.prototype.fromCToF = fromCToF;
+	Temperature.prototype.convert = function (amount) {
+		throw "Not implemented";
+		return {
+			from: function (F) {
+				return {
+					to: function (T) {
+					}
+				}
+			}
+		}
 	}
 
 	/*
@@ -173,183 +157,175 @@ var NumberEngine	= (function () {
 	 */
 
 	function fromKgToGr(n) { // KG -> GR
-
 		return (typeof n === "number") ? n * 1000 : NaN;
-
 	}
 
 	function fromKgToMg(n) { // KG -> GR -> MG
-
 		return (typeof n === "number") ? (n * 1000) * 1000 : NaN;
-
 	}
 
 	function fromKgToOz(n) { // KG -> GR -> OZ
-
 		return (typeof n === "number") ? (n * 1000) * GROZ : NaN;
-
 	}
 
 	function fromKgToLb(n) { // KG -> GR -> OZ -> LB
-
 		return (typeof n === "number") ? ((n * 1000) * GROZ) / 16 : NaN;
-
 	}
 
 	function fromKgToSt(n) { // KG -> GR -> OZ -> LB -> ST
-
 		return (typeof n === "number") ? (((n * 1000) * GROZ) / 16) / 14 : NaN;
-
 	}
 
 	function fromGrToKg(n) { // GR -> KG
-
 		return (typeof n === "number") ? n / 1000 : NaN;
-
 	}
 
 	function fromGrToMg(n) { // GR -> KG
-
 		return (typeof n === "number") ? n * 1000 : NaN;
-
 	}
 
 	function fromGrToOz(n) { // GR -> OZ
-
 		return (typeof n === "number") ? n * GROZ : NaN;
-
 	}
 
 	function fromGrToLb(n) { // GR -> OZ -> LB
-
 		return (typeof n === "number") ? (n * GROZ) / 16 : NaN;
-
 	}
 
 	function fromGrToSt(n) { // GR -> OZ -> LB -> ST
-
 		return (typeof n === "number") ? ((n * GROZ) / 16) / 14 : NaN;
-
 	}
 
 	function fromMgToKg(n) { // MG -> GR -> KG
-
 		return (typeof n === "number") ? (n / 1000) / 1000 : NaN;
-
 	}
 
 	function fromMgToGr(n) { // MG -> GR
-
 		return (typeof n === "number") ? n / 1000 : NaN;
-
 	}
 
 	function fromMgToOz(n) { // MG -> GR -> OZ
-
 		return (typeof n === "number") ? (n / 1000) * GROZ : NaN;
-
 	}
 
 	function fromMgToLb(n) { // MG -> GR -> OZ -> LB
-
 		return (typeof n === "number") ? ((n / 1000) * GROZ) / 16 : NaN;
-
 	}
 
 	function fromMgToSt(n) { // MG -> GR -> OZ -> LB -> ST
-
 		return (typeof n === "number") ? (((n / 1000) * GROZ) / 16) / 14 : NaN;
-
 	}
 
 	function fromOzToMg(n) { // OZ -> GR -> MG
-
 		return (typeof n === "number") ? (n * OZGR) * 1000 : NaN;
-
 	}
 
 	function fromOzToSt(n) { // OZ -> ST
-
 		return (typeof n === "number") ? (n / 16) / 14 : NaN;
-
 	}
 
 	function fromOzToLb(n) { // OZ -> LB
-
 		return (typeof n === "number") ? n / 16 : NaN;
-
 	}
 
 	function fromLbToKg(n) { //LB -> OZ -> GR -> KG
-
 		return (typeof n === "number") ? ((n * 16) * OZGR) / 1000 : NaN;
-
 	}
 
 	function fromLbToGr(n) { // LB -> OZ -> GR
-
 		return (typeof n === "number") ? (n * 16) * OZGR : NaN;
-
 	}
 
 	function fromLbToMg(n) { // LB -> OZ -> GR -> MG
-
 		return (typeof n === "number") ? ((n * 16) * OZGR) * 1000 : NaN;
-
 	}
 
 	function fromLbToSt(n) { // LB -> ST
-
 		return (typeof n === "number") ? n / 14 : NaN;
-
 	}
 
 	function fromLbToOz(n) { // LB -> OZ
-
 		return (typeof n === "number") ? n * 16 : NaN;
-
 	}
 
 	function fromStToKg(n) { // ST -> LB -> OZ -> GR -> KG
-
 		return (typeof n === "number") ? (((n * 14) * 16) * OZGR) / 1000 : NaN;
-
 	}
 
 	function fromStToGr(n) { // ST -> LB -> OZ -> GR
-
 		return (typeof n === "number") ? ((n * 14) * 16) * OZGR : NaN;
-
 	}
 
 	function fromStToMg(n) { // ST -> LB -> OZ -> GR -> MG
-
 		return (typeof n === "number") ? (((n * 14) * 16) * OZGR) * 1000 : NaN;
-
 	}
 
 	function fromStToOz(n) { // ST -> LB -> OZ
-
 		return (typeof n === "number") ? (n * 14) * 16 : NaN;
-
 	}
 
 	function fromStToLb(n) { // ST -> LB
-
 		return (typeof n === "number") ? n * 14 : NaN;
-
 	}
 
 	function fromOzToKg(n) { // OZ -> GR -> KG
-
 		return (typeof n === "number") ? (n * OZGR) / 1000 : NaN;
-
 	}
 
 	function fromOzToGr(n) { // OZ -> GR
-
 		return (typeof n === "number") ? n * OZGR : NaN;
+	}
 
+	function Weight() {
+		return weight || (this instanceof Weight ? weight = this : new Weight());
+	}
+
+	Weight.prototype.fromKgToGr = fromKgToGr;
+	Weight.prototype.fromKgToMg = fromKgToMg;
+	Weight.prototype.fromKgToOz = fromKgToOz;
+	Weight.prototype.fromKgToLb = fromKgToLb;
+	Weight.prototype.fromKgToSt = fromKgToSt;
+
+	Weight.prototype.fromGrToKg = fromGrToKg;
+	Weight.prototype.fromGrToMg = fromGrToMg;
+	Weight.prototype.fromGrToOz = fromGrToOz;
+	Weight.prototype.fromGrToLb = fromGrToLb;
+	Weight.prototype.fromGrToSt = fromGrToSt;
+
+	Weight.prototype.fromMgToKg = fromMgToKg;
+	Weight.prototype.fromMgToGr = fromMgToGr;
+	Weight.prototype.fromMgToOz = fromMgToOz;
+	Weight.prototype.fromMgToLb = fromMgToLb;
+	Weight.prototype.fromMgToSt = fromMgToSt;
+
+	Weight.prototype.fromOzToKg = fromOzToKg;
+	Weight.prototype.fromOzToGr = fromOzToGr;
+	Weight.prototype.fromOzToMg = fromOzToMg;
+	Weight.prototype.fromOzToLb = fromOzToLb;
+	Weight.prototype.fromOzToSt = fromOzToSt;
+
+	Weight.prototype.fromLbToKg = fromLbToKg;
+	Weight.prototype.fromLbToGr = fromLbToGr;
+	Weight.prototype.fromLbToMg = fromLbToMg;
+	Weight.prototype.fromLbToOz = fromLbToOz;
+	Weight.prototype.fromLbToSt = fromLbToSt;
+
+	Weight.prototype.fromStToKg = fromStToKg;
+	Weight.prototype.fromStToGr = fromStToGr;
+	Weight.prototype.fromStToMg = fromStToMg;
+	Weight.prototype.fromStToLb = fromStToLb;
+	Weight.prototype.fromStToOz = fromStToOz;
+
+	Weight.prototype.convert = function (amount) {
+		throw "Not implemented";
+		return {
+			from: function (F) {
+				return {
+					to: function (T) {
+					}
+				}
+			}
+		}
 	}
 
 	/*
@@ -357,156 +333,147 @@ var NumberEngine	= (function () {
 	 */
 
 	function fromKmToMt(n) { // KM -> MT
-
 		return (typeof n === "number") ? n * 1000 : NaN;
-
 	}
 
 	function fromKmToCm(n) { // KM -> MT -> CM
-
 		return (typeof n === "number") ? (n * 1000) * 100 : NaN;
-
 	}
 
 	function fromKmToMm(n) { // KM -> MT -> CM -> MM
-
 		return (typeof n === "number") ? ((n * 1000) * 100) * 10 : NaN;
-
 	}
 
 	function fromMtToKm(n) { // MT -> KM
-
 		return (typeof n === "number") ? (n / 1000) : NaN;
-
 	}
 
 	function fromMtToCm(n) { // MT -> CM
-
 		return (typeof n === "number") ? n * 100 : NaN;
-
 	}
 
 	function fromMtToMm(n) { // MT -> CM -> MM
-
 		return (typeof n === "number") ? (n * 100) * 10 : NaN;
-
 	}
 
 	function fromCmToKm(n) { // CM -> MT -> KM
-
 		return (typeof n === "number") ? (n / 100) / 1000 : NaN;
-
 	}
 
 	function fromCmToMt(n) { // CM -> MT
-
 		return (typeof n === "number") ? n / 100 : NaN;
-
 	}
 
 	function fromCmToMm(n) { // CM -> MM
-
 		return (typeof n === "number") ? n * 10 : NaN;
-
 	}
 
 	function fromMmToKm(n) { // MM -> CM -> MT -> KM
-
 		return (typeof n === "number") ? ((n / 10) / 100) / 1000 : NaN;
-
 	}
 
 	function fromMmToMt(n) { // MM -> CM -> MT
-
 		return (typeof n === "number") ? (n / 10) / 100 : NaN;
-
 	}
 
 	function fromMmToCm(n) { // MM -> CM
-
 		return (typeof n === "number") ? n / 10 : NaN;
-
 	}
 
 	function fromInToFt(n) { // IN -> FT
-
 		return (typeof n === "number") ? n / 12 : NaN;
-
 	}
 
 	function fromInToYd(n) { // IN -> FT -> YD
-
 		return (typeof n === "number") ? (n / 12) / 3 : NaN;
-
 	}
 
 	function fromInToMi(n) { // IN -> FT -> YD -> MI
-
 		return (typeof n === "number") ? ((n / 12) / 3) / 1760 : NaN;
-
 	}
 
 	function fromFtToIn(n) { // FT -> IN
-
 		return (typeof n === "number") ? n * 12 : NaN;
-
 	}
 
 	function fromFtToYd(n) { // FT -> YD
-
 		return (typeof n === "number") ? n / 3 : NaN;
-
 	}
 
 	function fromFtToMi(n) { // FT -> YD -> MI
-
 		return (typeof n === "number") ? (n / 3) / 1760 : NaN;
-
 	}
 
 	function fromYdToIn(n) { // YD -> FT -> IN
-
 		return (typeof n === "number") ? (n * 3) * 12 : NaN;
-
 	}
 
 	function fromYdToFt(n) { // YD -> FT
-
 		return (typeof n === "number") ? n * 3 : NaN;
-
 	}
 
 	function fromYdToMi(n) { // YD -> MI
-
 		return (typeof n === "number") ? n / 1760 : NaN;
+	}
 
+	function Distance() {
+		return distance || (this instanceof Distance ? distance = this : new Distance());
+	}
+
+	Distance.prototype.fromKmToMt = fromKmToMt;
+	Distance.prototype.fromKmToCm = fromKmToCm;
+	Distance.prototype.fromKmToMm = fromKmToMm;
+
+	Distance.prototype.fromMtToKm = fromMtToKm;
+	Distance.prototype.fromMtToCm = fromMtToCm;
+	Distance.prototype.fromMtToMm = fromMtToMm;
+
+	Distance.prototype.fromCmToKm = fromCmToKm;
+	Distance.prototype.fromCmToMt = fromCmToMt;
+	Distance.prototype.fromCmToMm = fromCmToMm;
+
+	Distance.prototype.fromMmToKm = fromMmToKm;
+	Distance.prototype.fromMmToMt = fromMmToMt;
+	Distance.prototype.fromMmToCm = fromMmToCm;
+
+	Distance.prototype.fromInToFt = fromInToFt;
+	Distance.prototype.fromInToYd = fromInToYd;
+	Distance.prototype.fromInToMi = fromInToMi;
+
+	Distance.prototype.fromFtToIn = fromFtToIn;
+	Distance.prototype.fromFtToYd = fromFtToYd;
+	Distance.prototype.fromFtToMi = fromFtToMi;
+
+	Distance.prototype.fromYdToIn = fromYdToIn;
+	Distance.prototype.fromYdToFt = fromYdToFt;
+	Distance.prototype.fromYdToMi = fromYdToMi;
+
+	Distance.prototype.convert = function (amount) {
+		throw "Not implemented";
+		return {
+			from: function (F) {
+				return {
+					to: function (T) {
+					}
+				}
+			}
+		}
 	}
 
 	function convert(amount) {
-
 		throw "Not implemented";
 		return {
-
 			from: function (F) {
-
 				return {
-
 					to: function (T) {
-
 					}
-
 				}
-
 			}
-
 		}
-
 	}
 
 	function NumberEngine() {
-
 		return numberEngine || (this instanceof NumberEngine ? numberEngine = this : new NumberEngine());
-
 	}
 
 	NumberEngine.prototype.fib = fib;
@@ -515,6 +482,7 @@ var NumberEngine	= (function () {
 	NumberEngine.prototype.fromHexToDec = fromHexToDec;
 	NumberEngine.prototype.fromOctToDec = fromOctToDec;
 
+	/*
 	NumberEngine.prototype.fromFToC = fromFToC;
 	NumberEngine.prototype.fromCToF = fromCToF;
 
@@ -581,6 +549,11 @@ var NumberEngine	= (function () {
 	NumberEngine.prototype.fromYdToIn = fromYdToIn;
 	NumberEngine.prototype.fromYdToFt = fromYdToFt;
 	NumberEngine.prototype.fromYdToMi = fromYdToMi;
+	*/
+
+	NumberEngine.prototype.Temperature = Temperature;
+	NumberEngine.prototype.Weight = Weight;
+	NumberEngine.prototype.Distance = Distance;
 
 	NumberEngine.prototype.convert = convert;
 
