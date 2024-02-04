e67758f81ea7d68dd45900a47fe126e05dd5bc4f
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-02-04 23:06:10 +0000

    Babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,23 +1,14 @@
-const presets = [
-  [
-    '@babel/env',
-    {
-      targets: {
-        node: 'current'
-      },
-      useBuiltIns: 'usage',
-      corejs: 3
-    }
-  ]
-]
-
-const plugins = [
-  '@babel/proposal-export-default-from',
-  '@babel/proposal-export-namespace-from',
-  '@babel/proposal-class-properties'
-]
-
 module.exports = {
-  presets,
-  plugins
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 'current'
+        },
+        useBuiltIns: 'usage',
+        corejs: 3
+      }
+    ]
+  ]
 }
